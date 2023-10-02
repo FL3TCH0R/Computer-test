303dac5f4cdccc8bb1af9d8dd035815788ca4ae4
FL3TCH0R <114429899+FL3TCH0R@users.noreply.github.com>
2023-10-02 13:16:14 -0400

    Add files via upload

---
+++ Question 2.py
@@ -0,0 +1,11 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def main():
+...     celcius=eval(input("Enter the temperature in celcius"))
+...     farenheit=9/5*celcius+32
+...     print("The temperature in farenheit is",farenheit,"degrees")
+... 
+...     
+>>> main()
+Enter the temperature in celcius 34
+The temperature in farenheit is 93.2 degrees
